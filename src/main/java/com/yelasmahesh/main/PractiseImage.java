15da7fa9c1f8c5f2ec1188e52cc578b1ed1bc9df
marikukula umamahesh <marikukula.umamahesh@r346-pos120.apple.com>
2020-10-13 13:32:34 +0530

    initial changes

---
+++ src/main/java/com/yelasmahesh/main/PractiseImage.java
@@ -0,0 +1,21 @@
+package com.yelasmahesh.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PractiseImage {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.add("Am in docker practise image");
+        list.forEach(item -> System.out.println(item));
+    }
+
+}
